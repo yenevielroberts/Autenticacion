0bdc1e1e197c14f4b3f373c11d0344b46ab96e30
Yeneviel Roberts <yeneviel.roberts.7e8@itb.cat>
2025-10-02 16:03:36 +0200

    Base de datos

--- index.js
+++ index.js
@@ -2,14 +2,18 @@ import express from 'express';
 import {PORT, SECRET_JWT_KEY} from './config.js'
 
 
-
 const app=express();
-app.use(express.json());
+
+app.use(express.json());//Se puede utilizar este en vez de bodyparse()
 app.use(express.static("public"))
-
-app.set('view engine', 'ejs')
-app
-
+app.set('view engine', 'ejs')//Quiere decir que quiero usar este motor
+app.set('views','./views')//Le digo donde estará las vistas
 
 
 app.listen(PORT,()=>console.log(`Server listening on http://localhost:${PORT}`))
+
+//Inicio de los endpoints
+
+app.get('/', (req, res)=>{
+    
+})
