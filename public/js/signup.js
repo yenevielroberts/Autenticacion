78de4c326f2ec1fc7725e1b1fac4a86ed3cdfed9
Yeneviel Roberts <yeneviel.roberts.7e8@itb.cat>
2025-10-09 16:38:16 +0200

    Cambio de formulario. Log in y sign un form juntos en la misma página

--- public/js/signup.js
+++ public/js/signup.js
@@ -1,44 +1,82 @@
-
 const $ = el => document.querySelector(el);
+        const loginForm = $('#login-form')
+        const loginSpan = $('#login-form span')
 
-const signupForm = $('#register-form');
-const signupSpan = $('#register-form span');
+        const registerForm = $('#register-form')
+        const registerSpan = $('#register-form span')
 
-//? comprueba si existe. applica el eventListener solo si existe
-signupForm?.addEventListener('submit', e => {
-    e.preventDefault()
-    const username = $('#register-username').value;
-    console.log(username)
-    const password = $('#register-password').value;
-    const confirmPassword = $('#register-confirm-password').value;
+        const logoutButton = $('#close-session')
 
-    if (password != confirmPassword) {
-        alert('Password do not match')
-        return
-    }
 
-    fetch('/signup', {
-        method: 'POST',
-        headers: {
-            'Content-Type': 'application/json'
-        },
-        body: JSON.stringify({ username, password })
-    })
-        .then(res => {//Obtengo la respuesta de parte del servidor
-            console.log(res)
+        loginForm?.addEventListener('submit', e => {
+            e.preventDefault()
+            const username = $('#login-username').value
+            const password = $('#login-password').value
 
-            if (res.ok) {
-                signupSpan.innerText = 'Usuario registrado. ..Entrando..'
-                signupSpan.style.color = 'green'
-                signupSpan.classList.add("success")
+            fetch('/login', {
+                method: 'POST',
+                headers: {
+                    'Content-Type': 'application/json'
+                },
+                body: JSON.stringify({ username, password })
+            })
+                .then(res => {
+                    if (res.ok) {
+                        loginSpan.innerText = 'Session iniciada ..Entrando..'
+                        loginSpan.style.color = 'green'
+                        setTimeout(() => {
+                            window.location.href = '/protected'
+                        }, 2000)
+                    } else {
+                        loginSpan.innerText = 'Error al iniciar session'
+                        loginSpan.style.color = 'red'
+                    }
+                })
+        });
 
-                setTimeout(()=>{
-                    window.location.href="/protected"
-                },2000)
-            } else {
-                signupSpan.innerText = 'Error registrado usuario'
-                signupSpan.style.color = 'red'
+        registerForm?.addEventListener('submit', e => {
+            e.preventDefault()
+            const username = $('#register-username').value
+            const password = $('#register-password').value
+            const confirmPassword = $('#register-confirm-password').value
+
+            if (password != confirmPassword) {
+                alert('Passwords do not match')
+                return
             }
-        })
 
-})
+            fetch('/register', {
+                method: 'POST',
+                headers: {
+                    'Content-Type': 'application/json'
+                },
+                body: JSON.stringify({ username, password })
+            })
+                .then(res => {
+                    console.log(res)
+                    if (res.ok) {
+                        registerSpan.innerText = 'Usuario registrado. ..Entrando..'
+                        registerSpan.style.color = 'green'
+                        setTimeout(() => {
+                            window.location.href = '/protected'
+                        }, 2000)
+                    } else {
+                        registerSpan.innerText = 'Error al registrar usuari'
+                        registerSpan.style.color = 'red'
+                    }
+                })
+        });
+
+        logoutButton?.addEventListener('click', e => {
+            e.preventDefault()
+            fetch('/logout', {
+                method: 'POST',
+                headers: {
+                    'Content-Type': 'application/json'
+                }
+            })
+                .then(res => {
+                    console.log(res)
+                    window.location.href = '/'
+                })
+        });
